de2c1e1ce7c5ac65c0bd7fdd282199c6ed795945
janalexandersson <janalexandersson97@gmail.com>
2019-04-07 17:44:01 +0200

    Added RPN program for the new stack class

---
+++ Lab3/RPNNoArray.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class RPNNoArray{
+	
+	
+	public static void evaluateRPN (String input){
+		StackNoArray stack = new StackNoArray();
+		// \\s+ means that we split on whitespace
+		for(String symbol : input.split("\\s+")){
+			switch(symbol){
+				case "+":
+					stack.push(stack.pop() + stack.pop());
+					break;
+				case "-":
+					stack.push( -stack.pop() + stack.pop());
+					break;
+				case "*":
+					stack.push(stack.pop() * stack.pop());
+					break;
+				case "/":
+					Double denom = stack.pop();
+					stack.push(stack.pop() / denom);
+					break;
+				case "=":
+					System.out.println("\nResult: " + stack.pop());
+					break;
+				case "q":
+					System.exit(0);
+				default:
+					stack.push(Double.parseDouble(symbol));
+					break;
+			}
+		}
+	}
+
+
+	public static void main(String[] args) {
+		String input;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("\nWrite \"q\" to quit");
+		
+		while(true){
+			System.out.println("\nWrite an RPN expression: ");
+			evaluateRPN(keyboard.nextLine());
+		}
+    }
+
+}
