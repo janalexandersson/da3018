53943d1b0a8234dd2bf4acfe3a2fc95ff9ec05ab
janalexandersson <janalexandersson97@gmail.com>
2019-04-22 14:09:13 +0200

    Implemented size and remove, some fixing more to do

--- Lab4/courseinfo/BinarySearchTree.java
+++ Lab4/courseinfo/BinarySearchTree.java
@@ -55,17 +55,35 @@ public class BinarySearchTree {
 	 * size: Count the number of nodes in the search tree
 	 */
 	
-	public int size() {
-	    return 0; // Dummy return value, to make it compile. Should be replace with proper algorithm.
+	public int sizeSub(BSTNode root){ //Size of subtree given a new root
+	
+		if(root == null){
+		
+			return 0;
+			
+		} else {
+		
+			return (sizeSub(root.getLeftChild()) + sizeSub(root.getRightChild()) + 1); 
+			//Using recursion to fins size of new subtrees until leaf.
+				
+		}
+	}
+	
+	public int size() { //Running recusrive size of subtrees
+	    
+		return sizeSub(root);
+		
 	}
 	
 	/**
 	 * find: Find a course given a course code
 	 */
 	 
+	 
+	 //HELP FUNCTION FOR FIND
 	 public BSTNode recursiveFind(String courseCode, BSTNode newRoot){
 			if (newRoot==null) {
-				System.out.println("Not in database");
+				//System.out.println("Not in database");
 				return null; 
 			} else {
 				if (newRoot.getCourseCode().compareTo(courseCode) > 0) { 
@@ -79,11 +97,72 @@ public class BinarySearchTree {
 			}
 		}
 	 
-	 
 	public BSTNode find(String courseCode) {
-		return recursiveFind(courseCode, root);
+		BSTNode possibleNode = recursiveFind(courseCode, root);
+		if ( possibleNode != null){
+			return possibleNode;
+		}else{
+			//System.out.println("Not in database");
+			throw new NullPointerException("Not in database");
+		}
 	}
 	
+	
+	
+	
+	
+	
+	private BSTNode findSmallestElem(BSTNode root) {
+		if (root.getLeftChild() == null)
+			return root;
+		else {
+			return findSmallestElem(root.getLeftChild());
+		}
+	}	
+	
+	
+	
+	private BSTNode removeNode(BSTNode root, String courseCode) {
+		if (root == null)
+			return null;
+		if (root.getCourseCode().compareTo(courseCode) > 0) {
+			root.setChildren(removeNode(root.getLeftChild(), courseCode), root.getRightChild());
+			
+		} else if (root.getCourseCode().compareTo(courseCode) < 0) {
+			root.setChildren(root.getLeftChild(), removeNode(root.getRightChild(), courseCode));
+			
+		} else {
+			// if nodeToBeDeleted have both children
+			if (root.getLeftChild() != null && root.getRightChild() != null) {
+				BSTNode temp = root;
+				// Finding minimum element from right
+				BSTNode minNodeForRight = findSmallestElem(temp.getRightChild());
+				// Replacing current node with minimum node from right subtree
+				root.courseCode = minNodeForRight.getCourseCode();
+				root.courseName = minNodeForRight.getCourseName();
+				root.credits = minNodeForRight.getCredits();
+				// Deleting minimum node from right now
+				removeNode(root.getRightChild(), minNodeForRight.getCourseCode());
+ 
+			}
+			// if nodeToBeDeleted has only left child
+			else if (root.getLeftChild() != null) {
+				root = root.getLeftChild();
+			}
+			// if nodeToBeDeleted has only right child
+			else if (root.getRightChild() != null) {
+				root = root.getRightChild();
+			}
+			// if nodeToBeDeleted do not have child (Leaf node)
+			else
+				root = null;
+		}
+		return root;
+	}
+	
+	public void remove(String courseCode) {
+		root = removeNode(root, courseCode);
+	}
 	
 	/**
 	 * Nodes in the search tree
--- Lab4/courseinfo/CourseInfo.java
+++ Lab4/courseinfo/CourseInfo.java
@@ -12,11 +12,20 @@ public class CourseInfo {
 		courses.insert("DA2004", "Programmeringsteknik", 7.5);
 		courses.insert("1", "1", 7.5);
 		courses.insert("2", "2", 7.5);
-		courses.insert("3", "3", 7.5);
+		courses.insert("7", "7", 7.5);
 		courses.insert("4", "4", 7.5);
+		courses.insert("3", "3", 7.5);
 		courses.insert("5", "5", 7.5);
 		
+		System.out.println(courses.find("4").getLeftChild().getCourseName());
+		System.out.println(courses.find("4").getRightChild().getCourseName());
+		System.out.println(courses.find("7").getLeftChild().getCourseName());
 		
+		courses.remove("4");
+		System.out.println(courses.find("4").getLeftChild().getCourseName());
+		//System.out.println(courses.find("4").getRightChild().getCourseName());
+		System.out.println(courses.find("7").getLeftChild().getCourseName());
+		/**
 		int n = courses.size();
 		System.out.printf("There are %d courses in the database.\n", n);
 		
@@ -24,7 +33,8 @@ public class CourseInfo {
 		
 		String name = courses.find("4").getCourseName();
 		System.out.printf("Name: %s\n", name);
-		System.out.println(courses.root.getCourseName());
+		System.out.println(courses.root.getCourseName());'
+		*/
 	}
 
 }
